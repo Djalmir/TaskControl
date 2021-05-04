d3fc3da8261f3bd4daaecb2ae63ebdf46483496f
Djalmir <djalmir_miodutzki@hotmail.com>
2021-05-04 17:35:22 -0300

    ultimos updates

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from 'axios'
 
 const api = axios.create({
@@ -33,39 +32,3 @@ export default {
 		return api.get(`/lists/${id}`)
 	}
 }
-=======
-import axios from 'axios'
-
-const api = axios.create({
-	baseURL: 'http://192.168.100.100:3000',
-	withCredentials: false,
-	headers: {
-		Accept: 'application/json',
-		'Content-Type': 'application/json'
-	}
-})
-
-export default {
-	getLists() {
-		return api.get(`/lists`)
-	},
-	postList(name) {
-		return api.post('/lists', {
-			name: name,
-			todos: []
-		})
-	},
-	putList(id, name, todos) {
-		return api.put(`/lists/${id}`, {
-			name: name,
-			todos: todos
-		})
-	},
-	deleteList(id) {
-		return api.delete(`/lists/${id}`)
-	},
-	getListById(id) {
-		return api.get(`/lists/${id}`)
-	}
-}
->>>>>>> 0428fe12c29612fb319cbdf6deb4efbbc0e0f9f1
